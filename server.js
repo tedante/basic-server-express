dce423c9d6784c0a38917b517834414e1ce96c0e
Teddy K <teddykoerniadi@gmail.com>
2024-03-26 11:51:15 +0700

    basic server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 3000;
 
 // defining the route for the root of the site
 app.get('/', (req, res) => {
-  // sending a response, in this case it is a string "Hello world!"
+  // sending a response to client, in this case it is a string "Hello world!"
   res.send('Hello World!');
 })
 
